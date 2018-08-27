27f2f8d534154c4bdedec75ea3f88d5a05980abf
bigwheel <k.bigwheel+eng@gmail.com>
2018-08-27 23:19:33 +0900

    Finish first implementation and tests

--- lib/yaml_strict_merge.rb
+++ lib/yaml_strict_merge.rb
@@ -1,9 +1,41 @@
 require "yaml_strict_merge/version"
 
+require 'yaml'
+
 module YamlStrictMerge
+  class YamlMergeError < StandardError; end
   class << self
-    def merge
-      1
+    def merge_yaml(lhs, rhs)
+      merge(YAML.load(lhs), YAML.load(rhs)).to_yaml
+    end
+
+    def merge(lhs, rhs)
+      # p lhs
+      # p lhs.class
+      # p rhs
+      # p rhs.class
+
+      return lhs unless rhs
+      return rhs unless lhs
+      raise YamlMergeError unless lhs.class == rhs.class
+      case lhs
+      when Hash
+        pairs = lhs.keys.concat(rhs.keys).to_set.map { |key|
+          [key, merge(lhs[key], rhs[key])]
+        }
+        return Hash[*pairs.flatten]
+      when Array
+        raise YamlMergeError unless lhs.size == rhs.size
+        return lhs.zip(rhs).map { |lv, rv|
+          merge(lv, rv)
+        }
+      else # primitive type
+        if lhs == rhs
+          return lhs
+        else
+          raise YamlMergeError
+        end
+      end
     end
   end
 end
--- spec/yaml_strict_merge_spec.rb
+++ spec/yaml_strict_merge_spec.rb
@@ -3,7 +3,62 @@ RSpec.describe YamlStrictMerge do
     expect(YamlStrictMerge::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(YamlStrictMerge::merge()).to eq(1)
+  it "merge simple shallow objects" do
+    expect(YamlStrictMerge::merge_yaml(<<~LHS, <<~RHS)).to eq(<<~EXPECT)
+      a: 1
+    LHS
+      b: 2
+    RHS
+      ---
+      a: 1
+      b: 2
+    EXPECT
+  end
+  it "merge objects recursively" do
+    expect(YamlStrictMerge::merge_yaml(<<~LHS, <<~RHS)).to eq(<<~EXPECT)
+      a:
+        f: 1
+        h: 3
+    LHS
+      a:
+        f: 1
+        g: 2
+    RHS
+      ---
+      a:
+        f: 1
+        h: 3
+        g: 2
+    EXPECT
+  end
+  it "throw exception when objects conflict" do
+    expect { YamlStrictMerge::merge_yaml(<<~LHS, <<~RHS) }.to raise_error(YamlStrictMerge::YamlMergeError)
+      a: 1
+    LHS
+      a: 2
+    RHS
+  end
+  it "merge same arrays" do
+    expect(YamlStrictMerge::merge_yaml(<<~LHS, <<~RHS)).to eq(<<~EXPECT)
+      - 1
+      - 2
+    LHS
+      - 1
+      - 2
+    RHS
+      ---
+      - 1
+      - 2
+    EXPECT
+  end
+  it "doesn't merge different length arrays" do
+    expect { YamlStrictMerge::merge_yaml(<<~LHS, <<~RHS) }.to raise_error(YamlStrictMerge::YamlMergeError)
+      - 1
+      - 2
+    LHS
+      - 1
+      - 2
+      - 3
+    RHS
   end
 end
